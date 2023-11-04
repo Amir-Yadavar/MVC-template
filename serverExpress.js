f6ca29801922d085f52174b24d06e2bfa9df1cf7
amir <amiresya@gmail.com>
2023-11-04 22:16:05 +0330

    express method post test ..

--- serverExpress.js
+++ serverExpress.js
@@ -6,6 +6,10 @@ server.get("/", (req, res) => {
     res.send("Hello world express")
 })
 
+server.post("/postmethod", (req, res) => {
+    res.send("this is post method")
+})
+
 server.listen(3000, () => {
     console.log("server runing in port 3000 successfuly");
 })
