af9e03c665b820b29292a1376eb99de9237fac4f
amir <amiresya@gmail.com>
2023-11-21 17:32:15 +0330

    user delete by express ..

--- serverExpress.js
+++ serverExpress.js
@@ -3,6 +3,8 @@ const server = express()
 server.use(express.json())
 require("./configs/db")
 const usersModel = require("./models/Users")
+const { default: mongoose } = require("mongoose")
+const { isValidObjectId } = require("mongoose")
 
 
 // server.get("/", (req, res) => {
@@ -24,6 +26,19 @@ server.post("/api/users", (req, res) => {
         })
         res.json("user successfuly")
     }
+})
+
+server.delete('/api/users/:id', async (req, res) => {
+    const { id } = req.params
+    if (isValidObjectId(id)) {
+        const userDelete = await usersModel.findByIdAndDelete({ _id: id })
+        if (!userDelete) {
+            return res.status(404).json("user not find")
+        }
+    } else {
+        return res.status(404).json("id not valid")
+    }
+    res.status(202).json("user deleted Successfully :)")
 })
 
 server.listen(3000, () => {
