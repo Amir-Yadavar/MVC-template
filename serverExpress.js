e21f4385fd4bef03e0cbe1fb53a5eb4086b8936c
amir <amiresya@gmail.com>
2024-04-10 16:49:06 +0330

    create token with jwt ..

--- serverExpress.js
+++ serverExpress.js
@@ -8,6 +8,13 @@ const { teachersModel } = require("./models/Teacher")
 const multer = require("multer")
 const uploader = require('./middlewares/multer')
 const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+
+const usersRouter = require("./routes/users")
+const booksRouter = require('./routes/book')
+const rentRouter = require("./routes/renst")
+const courseRouter = require("./routes/course")
+
 // server.post("/api/course/comment", async (req, res) => {
 //     // let teacher = await teachersModel.findOne({ _id: "65b2266cb3cf9e040bb78b6b" })
 //     // courseModel.create({
@@ -44,17 +51,25 @@ server.post('/manyUpload', uploader.array("profile", 3), (req, res) => {
 
 // hash and bcrypt pass
 const salt = bcrypt.genSaltSync(10)
-console.log(salt);
+// console.log(salt);
 
 const hashedPass = bcrypt.hashSync("amir123", salt)
-console.log(hashedPass);
+// console.log(hashedPass);
 
 
-const usersRouter = require("./routes/users")
-const booksRouter = require('./routes/book')
-const rentRouter = require("./routes/renst")
-const courseRouter = require("./routes/course")
+
+
 // const { teachersModel } = require("./models/Teacher")
+
+// create token
+const privateKey = "process.env.privateKey"
+const token = jwt.sign({ email: "amir@gmail.com" }, privateKey, {
+    algorithm: "HS256",
+    expiresIn: `${60} s`,
+})
+
+// console.log("token : ",token);
+// console.log(process.env.privateKey);
 
 server.use('/api/users', usersRouter)
 server.use("/api/books", booksRouter)
